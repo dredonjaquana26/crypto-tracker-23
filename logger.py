12082d36634ecdfc1b5331dc97c43a2c0b87ff6f
Dredon Jaquana <jaquanadredonbg@hotmail.com>
2026-09-25 00:06:45 +0200

    log rotation setup for crypto tracker

--- logger.py
+++ logger.py
@@ -2,29 +2,35 @@ import logging
 from logging.handlers import RotatingFileHandler
 import os
 
-def get_crypto_logger(name: str = 'crypto-tracker-23') -> logging.Logger:
-    log_path = os.path.join(os.getcwd(), 'logs', 'tracker.log')
-    os.makedirs(os.path.dirname(log_path), exist_ok=True)
+LOG_DIR = 'logs'
+LOG_FILE = 'crypto_tracker.log'
+
+def setup_crypto_logger():
+    if not os.path.exists(LOG_DIR):
+        os.makedirs(LOG_DIR)
+
+    logger = logging.getLogger('crypto-tracker-23')
+    logger.setLevel(logging.DEBUG)
 
     formatter = logging.Formatter(
-        '%(asctime)s | %(levelname)-8s | [%(name)s] -> %(message)s',
+        '[%(asctime)s] [%(levelname)s] [%(name)s] %(message)s',
         datefmt='%Y-%m-%d %H:%M:%S'
     )
 
-    handler = RotatingFileHandler(
-        log_path, 
-        maxBytes=5 * 1024 * 1024, 
+    file_handler = RotatingFileHandler(
+        os.path.join(LOG_DIR, LOG_FILE),
+        maxBytes=5 * 1024 * 1024,
         backupCount=3
     )
-    handler.setFormatter(formatter)
-
-    logger = logging.getLogger(name)
-    logger.setLevel(logging.DEBUG)
+    file_handler.setFormatter(formatter)
     
-    if not logger.handlers:
-        logger.addHandler(handler)
-        console = logging.StreamHandler()
-        console.setFormatter(formatter)
-        logger.addHandler(console)
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
 
+    if not logger.handlers:
+        logger.addHandler(file_handler)
+        logger.addHandler(console_handler)
+    
     return logger
+
+crypto_logger = setup_crypto_logger()
